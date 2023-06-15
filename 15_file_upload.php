99580cee23868c5408fe769e3eb219d34635d024
Ryan Azur <kodegowd64@gmail.com>
2023-06-15 11:25:41 +0800

    PHP Lectures

--- 15_file_upload.php
+++ 15_file_upload.php
@@ -1,13 +1,43 @@
 <?php
   /* ----------- File upload ---------- */
 
+$allowed_ext = array ('png', 'jpg', 'jpeg', 'gif');
+
   if (isset($_POST['submit'])) {
     //check if file was uploaded
-    if(!empty($_FILES['upload']['name']));
-  } else {
-    //if there is no file uploaded
+    if(!empty($_FILES['upload']['name'])) {
+      /* print_r($_FILES); */
+      $file_name = $_FILES['upload']['name'];
+      $file_size = $_FILES['upload']['size'];
+      $file_tmp = $_FILES['upload']['tmp_name'];
+      $target_dir = "uploads/" . $file_name;
+
+      //separating the file name (base name) and the extension
+      $file_ext = explode('.', $file_name);
+      $file_ext = strtolower(end($file_ext));
+
+      // echo $file_ext;
+
+      //Validate the file type/extension
+      if(in_array($file_ext, $allowed_ext)){
+        //Validate the file size
+        if($file_size <= 1048576) {   // 1MB limit
+          //Upload File
+          move_uploaded_file($file_tmp, $target_dir);
+
+          //Success message
+          echo '<p style="color: green;">File uploaded!</p>';
+        } else {
+          echo '<p style="color: red;">File too large!</p>';
+        }
+      } else {
+        $message = '<p style="color: red;">Invalid file type!</p>';
+      }     
+      
+    } else {    
     $message = '<p style="color: red;"> Please choose a file</p>';
   }
+  } 
   ?>
 
 <!DOCTYPE html>
--- 16_exceptions.php
+++ 16_exceptions.php
@@ -5,3 +5,54 @@
 /*
   PHP has an exception model similar to that of other programming languages. An exception can be thrown, and caught ("catched") within PHP. Code may be surrounded in a try block, to facilitate the catching of potential exceptions. Each try must have at least one corresponding catch or finally block.
 */
+
+function inverse($x) {
+  if (!$x) {
+    throw new Exception('Division by zero!');
+  }
+
+  return 1 / $x;
+}
+
+
+//Handle Exception
+
+/* try {
+  echo inverse(0) . '<br>';
+  echo inverse(5) . '<br>';
+  echo inverse(10) . '<br>';
+  
+} catch (Exception $e) {
+  echo 'Caught exception:', $e->getMessage(), '<br>';
+}
+
+echo 'Hello World'; */
+
+//With Finally
+try {
+  echo inverse(20) . '<br>';
+  echo inverse(0) . '<br>';
+  echo inverse(10) . '<br>';
+  
+} catch (Exception $e) {
+  echo 'Caught exception:', $e->getMessage(), '<br>';
+} finally {
+  echo 'Finally Kainan NA!' . '<br>';
+
+}
+
+
+try {
+  echo inverse(0) . '<br>';
+  echo inverse(5) . '<br>';
+  echo inverse(10) . '<br>';
+  
+} catch (Exception $e) {
+  echo 'Caught exception:', $e->getMessage(), '<br>';
+} finally {
+  echo 'Sana May Dessert' . '<br>';
+
+}
+
+echo 'Hello World';
+
